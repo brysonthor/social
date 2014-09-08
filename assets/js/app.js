2da9cc1426953b80aa72da3a9cf29475a6ccea14
Brandon Nicholls <brandon.nicholls@gmail.com>
2014-09-08 16:40:41 -0600

    Angular hitting service now

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,19 @@
 var app = angular.module('social', []);
 
-app.controller('test', ['$scope', 'friendService', function($scope, friendService) {
-  $scope.friends = friendService.getData();
-}]);
+app.factory('abc', function() {
+  return 'Brandon';
+});
 
-app.factory('friendService', function() {
-  var newService = {
-    data: ['a', 'b', 'zzz'],
-
-    getData: function () {
-      return data;
+app.factory('friendFactory', ['$http', function($http) {
+  return {
+    getFriends: function() {
+      return $http.get('/api/friends');
     }
   };
+}]);
 
-  return newService;
-});
+app.controller('test', ['$scope', 'friendFactory', function($scope, friendFactory) {
+  friendFactory.getFriends().success(function(data) {
+    $scope.friends = data.friends;
+  });
+}]);
