5d80289df5c33fba4592dfac01f0d24dd5381275
misbach <matt@misbach.org>
2014-11-07 16:48:00 -0700

    moving to new layout

--- assets/js/sharedPeople/sharedPeople.js
+++ assets/js/sharedPeople/sharedPeople.js
@@ -12,9 +12,8 @@ angular.module('social').directive('sharedPeople', ['sharedPeopleService', funct
         // Don't fetch data again if we already have it
         if ($('.people-list li').length > 0) return;
 
-        // Get user's people list
-        sharedPeopleService.getPeople(attrs.data).success(function(data) {
-          scope.people = data.taggedPerson;
+        sharedPeopleService.getPeople(attrs.data, function (data) {
+          scope.people = data;
         });
       });
 
--- assets/js/sharedPeople/sharedPeopleService.js
+++ assets/js/sharedPeople/sharedPeopleService.js
@@ -2,8 +2,26 @@ angular.module('social').service('sharedPeopleService', ['$http', function($http
 	$http.defaults.headers.common.Authorization = 'bearer '+FS.social.sessionId;
 
 	// Get list of memory people to share
-  this.getPeople = function(userId) {
-    return $http.get('/artifactmanager/patrons/'+userId+'/taggedPersons?maxRecords=999');
+  this.getPeople = function(userId, cb) {
+    // return $http.get('/artifactmanager/patrons/'+userId+'/taggedPersons?maxRecords=999');
+
+    $http.get('/artifactmanager/patrons/'+userId+'/taggedPersons?maxRecords=999').success(function(data) {
+      // Get shared people list
+      $http.get('api/share').success(function(rsp) {
+        // Iterate over shared people
+        for (var i=0; i<rsp.sharedPeople.length; i++) {
+          // Iterate people list
+          for (var j=0; j<data.taggedPerson.length; j++) {
+            // Mark those in people list who have been shared
+            if (data.taggedPerson[j].id == rsp.sharedPeople[i]) {
+              data.taggedPerson[j].selected = true;
+            }
+          }
+        }
+        // return data.taggedPerson;
+        cb(data.taggedPerson);
+      });
+    });
   };
 
 	// Save shared memory people
