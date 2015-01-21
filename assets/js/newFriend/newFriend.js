cca2388301b6ae277dcc5910656d395791204466
misbach <matt@misbach.org>
2015-01-21 14:32:50 -0700

    fixed src of sharing graphic

--- assets/js/newFriend/newFriend.js
+++ assets/js/newFriend/newFriend.js
@@ -7,6 +7,7 @@ angular.module('social').directive('newFriend', [function(newFriendService) {
     link: function(scope, element, attrs) {
       scope.name = attrs.name;
       scope.id = attrs.id;
+      scope.srcUrl = FS.File.img('sharinggraphic.svg');
       // Popup Modal when
       $('.new-friend-modal').modal('show');
     }
